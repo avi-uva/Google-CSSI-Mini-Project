2066a3def57923b5115adec3864ed05657b7fb3f
avi-uva <100551745+avi-uva@users.noreply.github.com>
2023-04-28 14:54:47 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,100 @@
+const PandaButton = document.getElementById("PandaButton");
+const LakersButton = document.getElementById("LakersButton");
+const SlidesButton = document.getElementById("SlidesButton");
+const OreoButton = document.getElementById("OreoButton");
+const DiorButton = document.getElementById("DiorButton");
+const RedButton = document.getElementById("RedButton");
+const KobeButton = document.getElementById("KobeButton");
+const ScottButton = document.getElementById("ScottButton");
+const DustyButton = document.getElementById("DustyButton");
+
+PandaButton.addEventListener("click", () => {
+  console.log("Panda Button has been clicked")
+  let isExecuted = confirm("Are you sure you want to purchase this sneaker?");
+  if (isExecuted == true) {
+    alert("Purchase Successful!");
+  }
+  else {
+    alert("Purchase Cancelled");
+  }
+})
+
+LakersButton.addEventListener("click", () => {
+  console.log("Lakers Button has been clicked")
+  let isExecuted = confirm("Are you sure you want to purchase this sneaker?");
+  if (isExecuted == true) {
+    alert("Purchase Successful!");
+  } else {
+    alert("Purchase Cancelled");
+  }
+})
+
+SlideButton.addEventListener("click", () => {
+  console.log("Slides Button has been clicked")
+  let isExecuted = confirm("Are you sure you want to purchase this sneaker?");
+  if (isExecuted == true) {
+    alert("Purchase Successful!");
+  } else {
+    alert("Purchase Cancelled");
+  }
+})
+
+OreoButton.addEventListener("click", () => {
+  console.log("Oreo Button has been clicked")
+  let isExecuted = confirm("Are you sure you want to purchase this sneaker?");
+  if (isExecuted == true) {
+    alert("Purchase Successful!");
+  } else {
+    alert("Purchase Cancelled");
+  }
+})
+
+DiorButton.addEventListener("click", () => {
+  console.log("Dior Button has been clicked")
+  let isExecuted = confirm("Are you sure you want to purchase this sneaker?");
+  if (isExecuted == true) {
+    alert("Purchase Successful!");
+  } else {
+    alert("Purchase Cancelled");
+  }
+})
+
+RedButton.addEventListener("click", () => {
+  console.log("Red Button has been clicked")
+  let isExecuted = confirm("Are you sure you want to purchase this sneaker?");
+  if (isExecuted == true) {
+    alert("Purchase Successful!");
+  } else {
+    alert("Purchase Cancelled");
+  }
+})
+
+KobeButton.addEventListener("click", () => {
+  console.log("Kobe Button has been clicked")
+  let isExecuted = confirm("Are you sure you want to purchase this sneaker?");
+  if (isExecuted == true) {
+    alert("Purchase Successful!");
+  } else {
+    alert("Purchase Cancelled");
+  }
+})
+
+DustyButton.addEventListener("click", () => {
+  console.log("Dusty Button has been clicked")
+  let isExecuted = confirm("Are you sure you want to purchase this sneaker?");
+  if (isExecuted == true) {
+    alert("Purchase Successful!");
+  } else {
+    alert("Purchase Cancelled");
+  }
+})
+
+ScottButton.addEventListener("click", () => {
+  console.log("Scott Button has been clicked")
+  let isExecuted = confirm("Are you sure you want to purchase this sneaker?");
+  if (isExecuted == true) {
+    alert("Purchase Successful!");
+  } else {
+    alert("Purchase Cancelled");
+  }
+})
